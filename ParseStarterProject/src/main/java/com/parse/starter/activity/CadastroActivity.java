101f8e4c5edad7c98fdadab62bbbeaf81f687a58
João Gabriel dos Santos Rodrigues <mailparajg@gmail.com>
2019-06-11 17:10:45 -0300

    [ADD/UPT] Novas implementações do instragam clone.

---
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/CadastroActivity.java
@@ -0,0 +1,15 @@
+package com.parse.starter.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.parse.starter.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
