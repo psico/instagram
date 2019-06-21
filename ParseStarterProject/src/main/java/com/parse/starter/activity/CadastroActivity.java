0dd40f51319d9107903875db46f16cdd28cff463
João Gabriel dos Santos Rodrigues <mailparajg@gmail.com>
2019-06-21 13:23:50 -0300

    [ADD] Implementado layout do login e cadastro de usuario.

--- ParseStarterProject/src/main/java/com/parse/starter/activity/CadastroActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/CadastroActivity.java
@@ -1,7 +1,9 @@
 package com.parse.starter.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.parse.starter.R;
 
@@ -11,5 +13,10 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+    }
+
+    public void abrirLoginUsuario(View view) {
+        Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
--- ParseStarterProject/src/main/java/com/parse/starter/activity/LoginActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.parse.starter.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.parse.starter.R;
 
@@ -11,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void abrirCadastroUsuario(View view) {
+        Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity(intent);
     }
 }
