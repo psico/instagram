971a2eda9b693a96240093a07095b531972b0f20
João Gabriel dos Santos Rodrigues <mailparajg@gmail.com>
2019-06-21 14:15:52 -0300

    [ADD] Inicio da implementação de tratamento de erros do parse.

--- ParseStarterProject/src/main/java/com/parse/starter/activity/CadastroActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/CadastroActivity.java
@@ -4,15 +4,62 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 import com.parse.starter.R;
+import com.parse.starter.activity.util.ParseErros;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText textoUsuario;
+    private EditText textoEmail;
+    private EditText textoSenha;
+    private Button botaoCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        textoUsuario = (EditText) findViewById(R.id.text_usuario);
+        textoEmail = (EditText) findViewById(R.id.text_email);
+        textoSenha = (EditText) findViewById(R.id.text_senha);
+        botaoCadastrar = (Button) findViewById(R.id.button_cadastrar);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cadastrarUsuario();
+            }
+        });
+    }
+
+    private void cadastrarUsuario() {
+
+        //Cria objeto usuario
+        ParseUser usuario = new ParseUser();
+        usuario.setUsername(textoUsuario.getText().toString());
+        usuario.setEmail(textoEmail.getText().toString());
+        usuario.setPassword(textoSenha.getText().toString());
+
+        //Salva dados do usuário
+        usuario.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(CadastroActivity.this, "Cadastro feito com sucesso!", Toast.LENGTH_LONG).show();
+                } else {
+                    ParseErros parseErros = new ParseErros();
+                    String erro = parseErros.getErro(e.getCode());
+                    Toast.makeText(CadastroActivity.this, erro, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public void abrirLoginUsuario(View view) {
