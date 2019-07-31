47eeefceab57d77f1aa0fecf20bec570a683a27f
João Gabriel <joao.rodrigues@embratur.gov.br>
2019-07-31 14:15:00 -0300

    [ADD] Adicionado icones da aplicação.

--- ParseStarterProject/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/parse/starter/R.java
+++ ParseStarterProject/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/parse/starter/R.java
@@ -2970,7 +2970,8 @@ public final class R {
     public static final int menu_main=0x7f0a0000;
   }
   public static final class mipmap {
-    public static final int ic_launcher=0x7f0b0000;
+    public static final int ic_instagram=0x7f0b0000;
+    public static final int ic_launcher=0x7f0b0001;
   }
   public static final class string {
     public static final int abc_action_bar_home_description=0x7f0c0000;
